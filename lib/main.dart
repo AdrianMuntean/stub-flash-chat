b02dd680a918a36ebdb2b94eac3f6a1c70533bfb
Adrian Muntean <adrian@sectorlabs.ro>
2020-03-04 22:11:43 +0200

    Fix main route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(FlashChat());
@@ -7,7 +7,7 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ChatScreen(),
+      home: WelcomeScreen(),
     );
   }
 }
